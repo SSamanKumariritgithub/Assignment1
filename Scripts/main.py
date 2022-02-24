dd43abbd363e722379473ea398f463869fcde6d7
Shammi <ar98369@fhss.sjp.ac.lk>
2022-02-24 21:47:12 +0530

    Create doubly linked list

--- Scripts/main.py
+++ Scripts/main.py
@@ -4,3 +4,7 @@ class Node:
         self.item = data
         self.next = None
         self.prev = None
+# Class for doubly Linked List
+class doublyLinkedList:
+    def __init__(self):
+        self.start_node = None
