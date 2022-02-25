0fea98592db9d57ce5a370e2d42d714c1722f901
Shammi <ar98369@fhss.sjp.ac.lk>
2022-02-25 13:04:11 +0530

    Insert Start

--- Scripts/main.py
+++ Scripts/main.py
@@ -11,9 +11,21 @@ class doublyLinkedList:
         self.start_node = None
 
 # Inseart Emptylist
-    def insert_in_emptylist(self, data):
+    def insert_in_emptylist(self,data):
         if self.start_node is None:
             new_node = Node(data)
             self.start_node = new_node
         else:
             print("list is not empty")
+
+# Insert start
+    def insert_at_start(self,data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+            print("node inserted")
+            return
+        new_node = Node(data)
+        new_node.nref = self.start_node
+        self.start_node.pref = new_node
+        self.start_node = new_node
