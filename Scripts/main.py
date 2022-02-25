b76b2387c4d78355570d7c93641adf5e22e5cc06
Shammi <ar98369@fhss.sjp.ac.lk>
2022-02-25 13:06:55 +0530

    Insert End

--- Scripts/main.py
+++ Scripts/main.py
@@ -29,3 +29,16 @@ class doublyLinkedList:
         new_node.nref = self.start_node
         self.start_node.pref = new_node
         self.start_node = new_node
+
+# Insert End
+def insert_at_end(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+            return
+        n = self.start_node
+        while n.nref is not None:
+            n = n.nref
+        new_node = Node(data)
+        n.nref = new_node
+        new_node.pref = n
