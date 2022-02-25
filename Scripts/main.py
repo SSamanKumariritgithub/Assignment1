d427fb1dba958b264805a8d338534e573d98ab3e
Shammi <ar98369@fhss.sjp.ac.lk>
2022-02-25 07:42:09 +0530

    Insert Empty list

--- Scripts/main.py
+++ Scripts/main.py
@@ -4,7 +4,16 @@ class Node:
         self.item = data
         self.next = None
         self.prev = None
+
 # Class for doubly Linked List
 class doublyLinkedList:
     def __init__(self):
         self.start_node = None
+
+# Inseart Emptylist
+    def insert_in_emptylist(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+        else:
+            print("list is not empty")
