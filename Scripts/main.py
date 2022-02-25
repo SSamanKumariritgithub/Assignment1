5b7a4226728b74648aa6c601506948dbfec93682
Shammi <ar98369@fhss.sjp.ac.lk>
2022-02-25 13:12:53 +0530

    Traversing

--- Scripts/main.py
+++ Scripts/main.py
@@ -84,3 +84,14 @@ def insert_before_item(self, x, data):
                 if n.pref is not None:
                     n.pref.nref = new_node
                 n.pref = new_node
+
+# Traversing
+def traverse_list(self):
+        if self.start_node is None:
+            print("List has no element")
+            return
+        else:
+            n = self.start_node
+            while n is not None:
+                print(n.item , " ")
+                n = n.nref
