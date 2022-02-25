0d77c356799f4a0be1eb07da0fc8ac7ac22246de
Shammi <ar98369@fhss.sjp.ac.lk>
2022-02-25 15:24:00 +0530

    create an empty linked list

--- Scripts/new.py
+++ Scripts/new.py
@@ -3,3 +3,9 @@ class Node:
   def __init__(self, data):
     self.data = data
     self.next = None
+
+#class Linked List
+class LinkedList:
+  #constructor to create an empty LinkedList
+  def __init__(self):
+    self.head = None
